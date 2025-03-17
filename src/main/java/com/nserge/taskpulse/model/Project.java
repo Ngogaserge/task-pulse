613c2c43aca53d1527cfcf2150dbab7d07b51f0d
Ngogaserge <sergengoga05@gmail.com>
2025-03-17 02:42:14 +0200

    initial commit

---
+++ src/main/java/com/nserge/taskpulse/model/Project.java
@@ -0,0 +1,10 @@
+package com.nserge.taskpulse.model;
+
+public class Project {
+    private int id;
+    private String name;
+    private String description;
+    private String color;
+    private String createdAt;
+    private String updatedAt;
+}
