3a138580de34a9f3265c37b61f31bae1aef9fcdf
Ngogaserge <sergengoga05@gmail.com>
2025-05-29 00:35:12 +0200

    add Project entity with fields and constructors

--- src/main/java/com/nserge/taskpulse/model/Project.java
+++ src/main/java/com/nserge/taskpulse/model/Project.java
@@ -1,10 +1,34 @@
 package com.nserge.taskpulse.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
 public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String description;
     private String color;
     private String createdAt;
     private String updatedAt;
+
+    public Project() {
+
+    }
+
+    public Project(int id, String name, String description, String color, String createdAt, String updatedAt) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.color = color;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
 }
